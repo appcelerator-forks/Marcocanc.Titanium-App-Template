506898ca946f653185a32fe1d3551b6d18447a43
Marco Cancellieri <marco@clever-apps.com>
2014-05-29 23:41:24 +0200

    added getJSON function to API

--- Resources/data/APIConnector.js
+++ Resources/data/APIConnector.js
@@ -1,8 +1,42 @@
 function APIConnector(settings) {
 	
-this.test = function(){
-	return "Working!";
-};
+	this.getJSON = function(url, data, callback){
+		if (typeof(data) == "function"){
+					callback = data;
+				}else if(data){
+					var datastring = "?";
+					var i = 0;
+					for (var name in data) {
+						if(i>0) datastring=datastring+"&";
+						datastring=datastring+name;
+						if(data[name])datastring=datastring+"="+data[name];
+						i++;
+					}
+					url = url+datastring;
+					
+		}
+		
+		var client = Ti.Network.createHTTPClient({
+			onload :function(e){
+				if(callback){
+					try{
+						var res = this.responseText;
+						var obj = JSON.parse(res);
+						callback(obj);
+					}
+					catch(err){
+						throw err;
+					}
+				}
+			},
+			onerror: function(err){
+				Ti.API.info(err);
+			}
+		});
+		client.open("GET", url);
+		client.send();
+	};
+	
 }
 
 
